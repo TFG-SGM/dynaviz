33c77bd08ffbcb04262066e20e6e8ef91a502df5
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2024-04-01 18:18:46 +0200

    add respoinsive to tests page

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c676b6d3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jo8klrqa7dg"
+    "revision": "0.vbepi9s31cg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
