e52ab66b037e100a58ac19f5bf672b112aa81ad9
Sergio Garcia <sergio.garciia.munoz@gmail.com>
2024-05-28 15:11:20 +0200

    fix some bugs

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-84e28d35'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pjt87ks9u2g"
+    "revision": "0.8iid6bhskqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
