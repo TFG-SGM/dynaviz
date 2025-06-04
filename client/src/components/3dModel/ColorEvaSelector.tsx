6278ada76ea6ce064758a05350e10c8d02ad10a5
Sergio García <sergio.garciia.munoz@gmail.com>
2025-06-04 17:02:18 +0200

    Fix build error

--- client/src/components/3dModel/ColorEvaSelector.tsx
+++ client/src/components/3dModel/ColorEvaSelector.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import { COLORS } from "../../utils/constants";
 import { adjustLightDark } from "../../utils/helpers";
 
